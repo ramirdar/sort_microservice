75e0de6371a894c0c50aead398100e8851bc12fa
ramirdar <71906014+ramirdar@users.noreply.github.com>
2023-02-13 16:50:38 -0800

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,68 @@
+import openai
+import json
+import time
+import os
+
+
+def dictToList(d):
+    return list(d.keys())
+            
+
+def listToString(s):
+    str1 = " "
+    return (str1.join(s))
+    
+def main():
+    while True:
+        #if file is empty do not conitnue
+        if os.path.getsize('/Users/darlita/CS361/PM/checklist.json') == 0:
+            print("File is empty!")
+            break
+
+        else:
+            #open file contents
+            sort_file = open('/Users/darlita/CS361/PM/checklist.json','r')
+            txt1 = json.load(sort_file)
+            sort_file.close()
+
+            #dict type
+            print(type(txt1), ";", txt1)
+            time.sleep(2)
+
+            #convert to list type 
+            new_list = (dictToList(txt1))
+            print(type(new_list),":",new_list)
+            
+
+            #convert list to str type
+            string = (listToString(new_list))
+            print(type(string),":", string)
+            
+            API_KEY = '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'
+            openai.api_key = API_KEY
+            model = 'text-davinci-003'
+            prompt = "randomly rank each item from most to least important. return it as a dict type with "" "+ string
+            response = openai.Completion.create(
+                prompt = prompt,
+                model = model,
+                temperature=0.1,
+                max_tokens=64,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0
+                
+            )
+            for result in response.choices:
+                time.sleep(2)
+                openai_result = result.text
+                
+                
+                file = open('/Users/darlita/CS361/PM/sort_file.json','w+')
+                file.write(openai_result)
+                file.close()
+                print(type(openai_result),";", openai_result)
+            break
+            
+
+if __name__ == '__main__':
+    main()
